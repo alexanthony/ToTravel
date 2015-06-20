d830520153f78f3c41ab4e874e5be4ae8140ea28
Alex Anthony <alex.anthony28991@gmail.com>
2015-06-21 09:25:12 +1200

    Added password retype check

---
+++ client/app/compareTo/compareTo.directive.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('toTravelApp')
+  .directive('compareTo', function () {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+        scope: {
+            otherModelValue: '=compareTo'
+        },
+        link: function(scope, element, attributes, ngModel) {
+             
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue === scope.otherModelValue;
+            };
+ 
+            scope.$watch('otherModelValue', function() {
+                ngModel.$validate();
+            });
+        }
+    };
+  });
